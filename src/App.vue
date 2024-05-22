f7013091df1c713c7ebb8b6023c4b8a4e5f34b67
Leonardo Parra <leonardoparrasilva2001@gmail.com>
2024-05-22 17:34:16 -0300

    alter: 17342205

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" alt="App Name" />
   <RouterView />
   <Footer />
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
+const logo_src = "/img/logo.png";
+const app_name = "Meu Aplicativo";
+
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,54 @@
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/logo"><img src="" id="logo" />Logo</RouterLink>
-        <RouterLink to="/pedidos">Pedidos</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/logo" id="logo-url">
+        <img :src="logo" :alt="alt" id="logo" />
+      </RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: "Navbar",
+  props: ["logo", "alt"]
 };
 </script>
 <style scoped>
+html {
+  margin: 0px;
+}
+
+nav {
+  display: flex;
+  background-color: rgba(255, 111, 111, 0.74);
+  border-bottom: 2px solid rgb(0, 0, 0);
+  margin: 0px;
+  justify-content: flex-end;
+}
+
+#logo-url {
+  padding: 10px;
+  margin: auto;
+  margin-left: 0;
+}
+
+#logo{
+  width: 40px;
+  height: 40px; 
+}
+
+nav a{
+  color: white ;
+  text-decoration: none;
+  align-content: center;
+  margin: 12px;
+  transition: .1s;
+}
+
+nav a:hover{
+  color: gray;
+}
 </style>
